f15b59286d1be2ba12002de8ddeb7df91e9f8326
chltmdgh522 <chltmdgh517@naver.com>
2025-01-05 18:22:12 +0900

    feat: 완성

--- comment/urls.py
+++ comment/urls.py
@@ -7,10 +7,10 @@ app_name = 'comment'
 
 urlpatterns = [
     # 댓글 작성
-    path('/comment/<int:pk>/create', comment_create, name='comment_create'),
+    path('comment/<int:pk>/create', comment_create, name='comment_create'),
 
     # 댓글 삭제
-    path('/comment/<int:pk>', comment_delete, name='comment_delete'),
+    path('comment/<int:pk>', comment_delete, name='comment_delete'),
 
 
 
